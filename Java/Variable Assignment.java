19833cade45e6c904853a03f26fc011c81e002c4
Lester <lester.deguzman@tup.edu.ph>
2019-08-21 11:45:55 +0800

    SW Problem 1

---
+++ Java/Variable Assignment.java
@@ -0,0 +1,20 @@
+package com.softieret;
+
+/*
+ Problem 1.
+ Write a Java Application that declares and assigns values to three int variables named x, y, and z.
+ Print out all three values, separated by commas, on a single line.
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x, y, z; // Declares variable x, y, z.
+        // Assign value to the variables.
+        x = 1;
+        y = 2;
+        z = 3;
+        // Print out the variables.
+        System.out.println(x+", "+y+ ", " +z);
+    }
+}
