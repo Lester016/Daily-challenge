4622fbde34a6abe852f708077ff834b12fa57db4
Lester <lester.deguzman@tup.edu.ph>
2019-08-21 11:56:35 +0800

    SW Problem 2

---
+++ Java/Unary Operator.java
@@ -0,0 +1,15 @@
+package com.softieret;
+
+/*
+ Problem 2.
+ What is the value of ~100? What is the value of ~-100? First try to figure it out,
+ and then write a program to print out the values.
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(~100); // Print out the value of minus the total positive value, which starts from 0.
+        System.out.println(~-100); // Print out the value of positive of total minus, positive starts from 0.
+    }
+}
