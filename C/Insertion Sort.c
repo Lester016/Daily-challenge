b948240edc487f3a14ff79932b5d4d55d54883a0
Lester016 <37885860+Lester016@users.noreply.github.com>
2019-10-06 12:52:08 +0800

    Update Insertion Sort.c

--- C/Insertion Sort.c
+++ C/Insertion Sort.c
@@ -11,14 +11,14 @@ int main() {
     }
     
     // Insertion algorithm.
-    for (int i = 0; i < arrSize; ++i) {
-        for (int j = 0; j < i; ++j) {
-            if (arr[i] < arr[j]) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
+    for (int i = 1; i < arrSize; ++i) {
+        for(int j = i; j > 0; j--) { 
+            if(arr[j - 1] > arr[j]){
+                int temp = arr[j];
+                arr[j] = arr[j - 1]; 
+                arr[j - 1] = temp; 
+             }
+         }
     }
 
     printf("\nSorted array: ");
