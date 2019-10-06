2cf46470e8d3ede4dca621a30afb941c68efe05f
Lester016 <37885860+Lester016@users.noreply.github.com>
2019-10-06 09:56:07 +0800

    Update Insertion Sort.c

--- C/Insertion Sort.c
+++ C/Insertion Sort.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    int myArray[8] = {4, 3, 2, 10, 12, 1, 5, 6}, i, j, k, temp, z;
-    for(i = 0; i <= 7; i++){
-        for(j = 0; j < i; j++){
-            if(myArray[i] < myArray[j]){
-                temp = myArray[j];
-                myArray[j] = myArray[i];
-                myArray[i] = temp;
-                continue;
+int main() {
+    printf("Insertion Sort\n");
+    int arr[] = {4, 3, 5, 6, 12, 2, 1, 8};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Given array: ");
+    for (int l = 0; l < arrSize; ++l) {
+        printf("%d ", arr[l]);
+    }
+    
+    // Insertion algorithm.
+    for (int i = 0; i < arrSize; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (arr[i] < arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
-    for(k = 0; k < i; k++){
-          printf("%d ", myArray[k]);
+
+    printf("\nSorted array: ");
+    for (int k = 0; k < arrSize; ++k) {
+        printf("%d ", arr[k]);
     }
 }
