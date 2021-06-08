785a28dc57be0fe3fd25e1ce5c3a8c5f721d8953
Lester016 <deguzmanlester.016@gmail.com>
2021-06-09 07:27:06 +0800

    Add files via upload

--- Python/nondirective-approach.py
+++ Python/nondirective-approach.py
@@ -1,82 +1,93 @@
 from random import randint
 
-
-greetings = "Good day. I hope you are well today.\nWhat can I do for you?\n"
-
-
-history = []
+greetings = "\033[0;1;34;40mGood day. I hope you are well today.\nWhat can I do for you?\n\033[1;31;40m"
 
 
-def response(message=""):
+class Doctor:
+    """
+    A Doctor class by:
+    Lester De Guzman & Hershey Delos Reyes
+    """
 
-    if message == 'quit':
-        return
+    def __init__(self):
+        self.history = []
 
-    pronouns = {
-        "I": "you",
-        "me": "you",
-        "myself": "yourself",
-        "I'm": "you are",
-        "My": "your",
-        "my": "your",
-        "you": "I",
-        "You": "I",
-        "Your": "my",
-        "your": "my",
-        "yours": "mine",
-        "mine": "yours",
-        "you're": "I'm",
-    }
+    def response(self, message=""):
 
-    updated_message = []
+        if message == 'quit':
+            return
 
-    splits = message.split()
+        pronouns = {
+            "I": "you",
+            "me": "you",
+            "myself": "yourself",
+            "I'm": "you are",
+            "My": "your",
+            "my": "your",
+            "you": "I",
+            "You": "I",
+            "Your": "my",
+            "your": "my",
+            "yours": "mine",
+            "mine": "yours",
+            "you're": "I'm",
+        }
 
-    # for loop to iterate over words array
-    for word in splits:
-        if word in pronouns:
-            updated_message.append(pronouns[word])
+        updated_message = []
+
+        splits = message.split()
+
+        # for loop to iterate over words array
+        for word in splits:
+            if word in pronouns:
+                updated_message.append(pronouns[word])
+            else:
+                updated_message.append(word)
+
+        updated_message = " ".join(word for word in updated_message)
+
+        qualifier = [
+            "Why do you say that",
+            "You seem to think that",
+            "Can you explain why"
+        ]
+
+        hedge = [
+            "Please tell me more",
+            "What happened then?",
+            "You sound sad.",
+            "I'd like to hear your opinion.",
+            "How will you handle this in the future?",
+            "It sounds to me like you're feeling overwhelmed right now.",
+        ]
+
+        temp_rand = randint(0, len(qualifier) + len(hedge))
+        if len(self.history) >= 4:
+            # Repeat if random generated number already in history.
+            while temp_rand in self.history:
+                temp_rand = randint(0, len(qualifier) + len(hedge))
+
+            del self.history[0]
+            self.history.append(temp_rand)
         else:
-            updated_message.append(word)
+            while temp_rand in self.history:
+                temp_rand = randint(0, len(qualifier) + len(hedge))
 
-    updated_message = " ".join(word for word in updated_message)
+            self.history.append(temp_rand)
 
-    qualifier = [
-        "Why do you say that",
-        "You seem to think that",
-        "Can you explain why"
-    ]
+        current_number = self.history[len(self.history) - 1]
 
-    hedge = [
-        "Please tell me more",
-        "What happened then?",
-        "You sound sad.",
-        "I'd like to hear your opinion.",
-        "How will you handle this in the future?",
-        "It sounds to me like you're feeling overwhelmed right now.",
-    ]
-
-    temp_rand = randint(0, len(qualifier) + len(hedge))
-    if len(history) >= 4:
-        # Repeat if random generated number already in history.
-        while temp_rand in history:
-            temp_rand = randint(0, len(qualifier) + len(hedge))
-
-        del history[0]
-        history.append(temp_rand)
-    else:
-        while temp_rand in history:
-            temp_rand = randint(0, len(qualifier) + len(hedge))
-
-        history.append(temp_rand)
-    
-    current_number = history[len(history) - 1]
-
-    # If latest history is 1, use qualifier.
-    if current_number <= 3:
-        response(input(f"{qualifier[current_number -1]} {updated_message}\n"))
-    else:
-        response(input(f"{hedge[current_number - 4]}\n"))
+        # If latest history is 1, use qualifier.
+        if current_number <= 3:
+            self.response(
+                input(f"\033[0;1;34;40m{qualifier[current_number -1]} {updated_message}\n\033[1;31;40m"))
+        else:
+            self.response(
+                input(f"\033[0;1;34;40m{hedge[current_number - 4]}\n\033[1;31;40m"))
 
 
-response(input(greetings))
+message = input(greetings)
+
+doctor_1 = Doctor()
+
+doctor_1.response(message)
